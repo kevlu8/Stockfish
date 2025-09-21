a180e2a810b319c2eacca828a29c12cdeb0f2044
Kevin Lu <kevie.lu8@gmail.com>
2025-09-20 20:04:32 -0700

    Use an adjustable bound based on depth difference for step 12
    Bench: 2594526

--- src/search.cpp
+++ src/search.cpp
@@ -959,7 +959,7 @@ Value Search::Worker::search(
 moves_loop:  // When in check, search starts here
 
     // Step 12. A small Probcut idea
-    probCutBeta = beta + 418;
+    probCutBeta = beta + std::max(104, 104 * (depth - ttData.depth));
     if ((ttData.bound & BOUND_LOWER) && ttData.depth >= depth - 4 && ttData.value >= probCutBeta
         && !is_decisive(beta) && is_valid(ttData.value) && !is_decisive(ttData.value))
         return probCutBeta;
