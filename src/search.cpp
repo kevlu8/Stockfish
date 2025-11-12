5f5ff41fcfcb27c65046b765b92ca94735e3e273
Kevin Lu <69993704+kevlu8@users.noreply.github.com>
2025-11-12 22:54:50 +0000

    Change RFP formula to use (beta + eval) / 2
    Bench: 2864415

--- src/search.cpp
+++ src/search.cpp
@@ -857,7 +857,7 @@ Value Search::Worker::search(
 
         if (!ss->ttPv && depth < 14 && eval - futility_margin(depth) >= beta && eval >= beta
             && (!ttData.move || ttCapture) && !is_loss(beta) && !is_win(eval))
-            return (2 * beta + eval) / 3;
+            return (beta + eval) / 2;
     }
 
     // Step 9. Null move search with verification search
