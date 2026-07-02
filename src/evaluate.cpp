bf62bb6155be522eb561a1176d9076375b54858e
Kevin Lu <kevie.lu8@gmail.com>
2026-07-02 11:55:13 -0600

    Try bringing back the NNUE weighing but skewing in the opposite direction
    Bench: 2609294

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -48,7 +48,7 @@ Value Eval::evaluate(const Eval::NNUE::Network&     network,
 
     auto [psqt, positional] = network.evaluate(pos, accumulators, caches);
 
-    Value nnue = psqt + positional;
+    Value nnue = (131 * psqt + 125 * positional) / 128;
 
     // Blend optimism and eval with nnue complexity
     int nnueComplexity = std::abs(psqt - positional);
